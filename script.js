0e7295cd3063b2615055ad15efe936869d6950c1
Beingkunalistic <kunalkapoor2604@gmail.com>
2024-01-04 09:54:06 +0530

    updated script, made some changes in style.css and added a new section.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // console.log("entered")
-    const fadeUpElements = document.querySelectorAll('.intro-left');
-  
-    fadeUpElements.forEach(element => {
-      element.classList.add('active');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // console.log("entered")
+  const fadeUpElements = document.querySelectorAll('.intro-left');
+
+  fadeUpElements.forEach(element => {
+    element.classList.add('active');
   });
+});
+function scrollAbout() {
+  document.getElementById('about').scrollIntoView({ behavior: "smooth" });
+}
