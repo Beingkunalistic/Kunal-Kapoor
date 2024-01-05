e225ca4e9ad2200465891b3a821522c30c0e8298
Beingkunalistic <kunalkapoor2604@gmail.com>
2024-01-06 00:31:47 +0530

    Important updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Script to add active class to the intro section so it fades up.
 document.addEventListener("DOMContentLoaded", function () {
   // console.log("entered")
   const fadeUpElements = document.querySelectorAll('.intro-left');
@@ -6,6 +7,45 @@ document.addEventListener("DOMContentLoaded", function () {
     element.classList.add('active');
   });
 });
+
+//function to scroll the page upon clicking the down arrow.
 function scrollAbout() {
-  document.getElementById('about').scrollIntoView({ behavior: "smooth" });
+  var targetSection = document.getElementById('about');
+  var offset = 600;
+  targetSection.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  window.scrollBy(0, offset);
 }
+
+
+//script for animating text.
+document.addEventListener('DOMContentLoaded', function () {
+  const words = ['Student', 'Web Developer', 'Musician', 'Tech Enthusiast!'];
+  let index = 0;
+  let letterIndex = 0;
+  let currentWord = '';
+
+  function type() {
+    if (letterIndex < words[index].length) {
+      currentWord += words[index][letterIndex];
+      document.getElementById('dynamic-text').innerText = currentWord;
+      letterIndex++;
+      setTimeout(type, 100); // Adjust the typing speed
+    } else {
+      setTimeout(erase, 1000); // Wait for a second before erasing
+    }
+  }
+
+  function erase() {
+    if (letterIndex > 0) {
+      currentWord = currentWord.slice(0, -1);
+      document.getElementById('dynamic-text').innerText = currentWord;
+      letterIndex--;
+      setTimeout(erase, 50); // Adjust the erasing speed
+    } else {
+      index = (index + 1) % words.length;
+      setTimeout(type, 500); // Wait for half a second before typing the next word
+    }
+  }
+
+  type(); // Start the typing animation
+});
